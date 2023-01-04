e987f98e01f17200ac3be857123ad8578e2c1404
yoondgu <97426362+yoondgu@users.noreply.github.com>
2023-01-05 04:58:59 +0900

    [Bronze V] Title: 사분면 고르기, Time: 204 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        
+        sc.close();
+                
+        if (x > 0 && y > 0) {
+            System.out.println(1);
+        }
+        if (x < 0 && y > 0) {
+            System.out.println(2);
+        }
+        if (x < 0 && y < 0) {
+            System.out.println(3);
+        }
+        if (x > 0 && y < 0) {
+            System.out.println(4);
+        }
+    }
+}
+    
