ebfef7bdd3bdb3ab699ec56c2e5419d286263495
Doy <doyoungwork@gmail.com>
2023-07-01 10:28:43 +0900

    [Bronze II] Title: 분해합, Time: 224 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        final Scanner scanner = new Scanner(System.in);
+        final int N = Integer.parseInt(scanner.nextLine());
+
+        int M = 0;
+        int x = N;
+        while (x > 1) {
+            if (N == x + sumAllDigits(x)) {
+                M = x;
+            }
+            x--;
+        }
+
+        System.out.println(M);
+    }
+
+    private static int sumAllDigits(int number) {
+        int result = 0;
+        while (number > 0) {
+            result += number%10;
+            number /= 10;
+        }
+        return result;
+    }
+}
