6e55a87ca5592a787d2d6cec6b5bf382b4f2893b
Doyoung Yoo <doyoungwork@gmail.com>
2023-10-30 00:08:24 +0900

    [Bronze II] Title: 공 바꾸기, Time: 128 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -12,22 +12,20 @@ public class Main {
         int N = Integer.parseInt(tk.nextToken());
         int M = Integer.parseInt(tk.nextToken());
 
-        int[] bags = new int[N];
+        String[] bags = new String[N];
         for (int i = 0; i < N; i++) {
-            bags[i] = i+1;
+            bags[i] = String.valueOf(i+1);
         }
 
         for (int i = 0; i < M; i++) {
             StringTokenizer balls = new StringTokenizer(br.readLine(), " ");
             int first = Integer.parseInt(balls.nextToken());
             int second = Integer.parseInt(balls.nextToken());
-            int temp = bags[first-1];
+            String temp = bags[first-1];
             bags[first-1] = bags[second-1];
             bags[second-1] = temp;
         }
 
-        for (int bag : bags) {
-            System.out.print(bag + " ");
-        }
+        System.out.println(String.join(" ", bags));
     }
 }
