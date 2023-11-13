e79bed86f965113d1dbb1a2c4e32a7467f54e125
Doyoung Yoo <doyoungwork@gmail.com>
2023-11-13 13:59:13 +0900

    [Silver V] Title: 숫자 카드, Time: 1640 ms, Memory: 321760 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -2,42 +2,51 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 
-    static int N;
-    static int M;
-    static int[] cards;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        cards = Arrays.stream(br.readLine().split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
-        Arrays.sort(cards);
+        int N = Integer.parseInt(br.readLine());
+        final List<Integer> cards = Arrays.stream(br.readLine().split(" "))
+                .map(Integer::parseInt)
+                .sorted()
+                .collect(Collectors.toList());
+        int M = Integer.parseInt(br.readLine());
+        final List<Integer> toFinds = Arrays.stream(br.readLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
 
-        M = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        while (st.hasMoreTokens()) {
-            System.out.print(binarySearch(Integer.parseInt(st.nextToken()), 0, N - 1) + " ");
-        }
-    }
-
-    private static int binarySearch(int key, int low, int high) {
-        int mid;
-
-        while (low <= high) {
-            mid = (low + high) / 2;
-            if (key == cards[mid]) {
-                return 1;
-            } else if (key < cards[mid]) {
-                high = mid - 1;
+        StringBuilder sb = new StringBuilder();
+        for (int target : toFinds) {
+            if (find(cards, target)) {
+                sb.append("1 ");
             } else {
-                low = mid + 1;
+                sb.append("0 ");
             }
         }
-        return 0;
+        System.out.println(sb);
+    }
+
+    static boolean find(List<Integer> cards, int target) {
+        if (cards.isEmpty()) {
+            return false;
+        }
+        final int cardsSize = cards.size();
+        final int mid = cardsSize/2;
+        final int midValue = cards.get(mid);
+
+        if (mid == 0 & midValue != target) {
+            return false;
+        }
+        if (midValue == target) {
+            return true;
+        } else if (midValue < target) {
+            return find(cards.subList(mid, cardsSize), target);
+        } else {
+            return find(cards.subList(0, mid), target);
+        }
     }
 }
