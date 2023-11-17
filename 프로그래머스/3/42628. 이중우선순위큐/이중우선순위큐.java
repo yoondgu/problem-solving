ffebe16723f000d3ce1713c69b9c982db30e10c1
Doyoung Yoo <doyoungwork@gmail.com>
2023-11-17 14:34:10 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.17 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -3,38 +3,51 @@ import java.lang.*;
 
 class Solution {
     public int[] solution(String[] operations) {
-        PriorityQueue<Integer> minQueue = new PriorityQueue<>();
-        PriorityQueue<Integer> maxQueue = new PriorityQueue<>(
-            (a, b) -> (-1)*Integer.compare(a, b)
-        );
+        Deque<Integer> deque = new ArrayDeque<>();
         
         for (String operation : operations) {
             // 삽입 -> 삽입할 때마다 정렬되어야 한다.
-            if (operation.startsWith("I")) {
+            if (operation.startsWith("I")) {                
                 int value = Integer.parseInt(operation.split(" ")[1]);
-                minQueue.add(value);
-                maxQueue.add(value);
+                if (deque.isEmpty()) {
+                    deque.addFirst(value);
+                    continue;
+                }
+                // 4 -> 1이 들어오면 앞에 넣기
+                int first = deque.peekFirst();
+                if (first >= value) {
+                    deque.addFirst(value);
+                    continue;
+                }
+                // 1 4 -> 3이 들어오면? 4를 빼고 3 넣고 4 넣기
+                int last = deque.peekLast();
+                if (last <= value) {
+                    deque.addLast(value);                    
+                    continue;
+                }
+                deque.pollLast();
+                deque.addLast(value);
+                deque.addLast(last);                
             }
-            if (minQueue.isEmpty()) {
-                continue;
+
+            if ("D 1".equals(operation) && !deque.isEmpty()) {
+                deque.pollLast();
+                
             }
-            if ("D 1".equals(operation)) {
-                int max = maxQueue.poll();
-                minQueue.remove(max);
-            }
-            if ("D -1".equals(operation)) {
-                int min = minQueue.poll();
-                maxQueue.remove(min);
+            if ("D -1".equals(operation) && !deque.isEmpty()) {
+                deque.pollFirst();
+                
             }
         }
-        
-        if (minQueue.isEmpty()) {
+                
+        if (deque.isEmpty()) {
             return new int[]{0, 0};
         }
         
         int[] answer = new int[2];
-        answer[0] = maxQueue.peek();
-        answer[1] = minQueue.peek();
+        answer[0] = deque.getLast();
+        answer[1] = deque.getFirst();
+        
         return answer;
     }
 }
