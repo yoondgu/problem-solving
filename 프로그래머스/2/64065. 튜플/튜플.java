afdfe80072ef097a4a11ec29f73bfb61e5ef3387
Doyoung Yoo <doyoungwork@gmail.com>
2023-11-30 11:06:34 +0900

    [level 2] Title: 튜플, Time: 0.71 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -9,24 +9,31 @@ class Solution {
         
         int size = sets.length;
         int[] answer = new int[size];
+        boolean[] isCounted = new boolean[1_000_001];
         
         
-        // 한개만 들어있는 원소를 먼저 찾는다.
-        // 그다음은 두개, 세개, ...
-        Set<Integer> results = new LinkedHashSet<>();
         for (int i = 1; i <= size; i++) {
+            boolean find = false;
             for (String set : sets) {
                 String[] trimed = set.replace("{", "").replace("}", "").split(",");
                 if (trimed.length == i) {
                     for (String trim : trimed) {
-                        results.add(Integer.parseInt(trim));
+                        int value = Integer.parseInt(trim);
+                        if (isCounted[value]) {
+                            continue;
+                        }
+                        answer[i-1] = value;
+                        isCounted[value] = true;     
+                        find = true;
+                        break;
                     }
+                }
+                if (find) {
+                    continue;
                 }
             }
         }
         
-        return results.stream()
-            .mapToInt(Number::intValue)
-            .toArray();
+        return answer;
     }
 }
